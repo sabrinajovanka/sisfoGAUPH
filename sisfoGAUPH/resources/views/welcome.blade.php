68a6dda51207fc5c3dd5038b42d620d4d78d999e
sabrinajovanka <sabrinaalwin19@gmail.com>
2022-11-20 04:20:48 +0700

    first commit

---
+++ sisfoGAUPH/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title')
+    Home
+@endsection
+@section('content')
+    <div class="container">
+        <div class="row">
+            {{-- @include('admin.sidebar') --}}
+
+            <div class="col-md-9">
+                <div class="card">
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
